03e9a64ac3b907c7fd98de2044d08f6191562bf8
Roy Henriquez <roy.henriquez@gmail.com>
2020-05-03 17:31:39 -0500

    Added user module and google sign in directive

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { UserRoutingModule } from "./user-routing.module";
+import { SharedModule } from "../shared/shared.module";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { GoogleSigninDirective } from "./google-signin.directive";
+import { ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
+	declarations: [LoginPageComponent, GoogleSigninDirective],
+	imports: [CommonModule, UserRoutingModule, SharedModule, ReactiveFormsModule]
+})
+export class UserModule {}

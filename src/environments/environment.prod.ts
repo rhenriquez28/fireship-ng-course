5e941a3880c3c08c384f64ddc777a0dd0f400b79
Roy Henriquez <roy.henriquez@gmail.com>
2020-05-03 12:13:25 -0500

    Added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyBia20EfrHW97BQHsXR59es5fx6fZvUnes",
+		authDomain: "test-9da4b.firebaseapp.com",
+		databaseURL: "https://test-9da4b.firebaseio.com",
+		projectId: "test-9da4b",
+		storageBucket: "test-9da4b.appspot.com",
+		messagingSenderId: "597375216859",
+		appId: "1:597375216859:web:09018d81ac21279877b0fe"
+	}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firebase: {
+		apiKey: "AIzaSyBia20EfrHW97BQHsXR59es5fx6fZvUnes",
+		authDomain: "test-9da4b.firebaseapp.com",
+		databaseURL: "https://test-9da4b.firebaseio.com",
+		projectId: "test-9da4b",
+		storageBucket: "test-9da4b.appspot.com",
+		messagingSenderId: "597375216859",
+		appId: "1:597375216859:web:09018d81ac21279877b0fe"
+	}
 };
 
 /*
